48467488dc7369c3da934b55d7df19ef53f7a53f
thosaphol <thosaphol@outlook.co.th>
2026-03-03 22:55:32 +0700

    complete sample docker compose with mysql

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
-
+const mysql = require('mysql2/promise');
 const app = express();
 const PORT = 3000;
+let conn = null
+
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: 'db', // หรือใส่เป็น localhost ก็ได้
+    user: 'root',
+    password: 'root',
+    database: 'tutorial'
+  })
+}
 
 app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello, World!' });
 });
 
-app.listen(PORT, () => {
+app.get('/users', async (req, res) => {
+  const [results] = await conn.query('SELECT * FROM users')
+  res.json(results)
+})
+
+app.listen(PORT, async () => {
+    await initMySQL();
     console.log(`Server running on http://localhost:${PORT}`);
 });
